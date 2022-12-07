be2059772d45a6066ddb63e53ef646ac4d960607
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-12-08 00:01:35 +0100

    add recipe list and item component, add icon and axios dependency

---
+++ src/components/recipe/recipeItem/RecipeItem.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './recipeitem.scss'
+
+export const RecipeItem = () => {
+  return (
+    <article>RecipeItem</article>
+  )
+}
---
+++ src/components/recipe/recipeList/RecipeList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { RecipeItem } from "../recipeItem/RecipeItem";
+import "./recipelist.scss";
+
+export const RecipeList = () => {
+  return (
+    <section>
+      <RecipeItem />
+    </section>
+  );
+};
