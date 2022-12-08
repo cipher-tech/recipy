b2aeb6d3b9179a4d553fe2dc5a33e3681efacb17
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-12-08 22:25:58 +0100

    render recipe items through the recipe list comp on the recipe page

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { Fragment } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home/Home";
 import { RecipeDetail } from "./pages/recipeDetail/RecipeDetail";
-import { RecipesList } from "./pages/recipesList/RecipesList";
+import { Recipes } from "./pages/recipes/Recipes";
 
 export const App = () => {
   return (
     <Fragment>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/recipes" element={<RecipesList />} />
+        <Route path="/recipes" element={<Recipes />} />
         <Route path="/detail" element={<RecipeDetail />} />
       </Routes>
     </Fragment>
--- src/pages/recipesList/RecipesList.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const RecipesList = () => {
-  return <div>RecipesList</div>;
-};
