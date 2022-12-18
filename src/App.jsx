7dd8eb0ea75d5594bbe7d4e908fbf2e3c6f8cdff
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-12-18 10:16:41 +0100

    modify structure to allow pages house pages and its routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,10 @@
 import { Fragment } from "react";
-import { Route, Routes } from "react-router-dom";
-import { Layout } from "../Layout";
-//pages
-import { Home } from "./pages/home/Home";
-import { RecipeDetail } from "./pages/recipeDetail/RecipeDetail";
-import { RecipesPage } from "./pages/recipesPage/RecipesPage";
-//context
-import { RecipeProvider } from "./context/recipe/RecipeProvider";
+
+import { Pages } from "./pages/Pages";
 export const App = () => {
   return (
     <Fragment>
-      <RecipeProvider>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="/" index element={<Home />} />
-            <Route path="recipes" element={<RecipesPage />} />
-            <Route path="recipes/:id" element={<RecipeDetail />} />
-          </Route>
-        </Routes>
-      </RecipeProvider>
+      <Pages />
     </Fragment>
   );
 };
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "../../Layout";
+//pages
+import { Home } from "./home/Home";
+import { RecipeDetail } from "./recipeDetail/RecipeDetail";
+import { RecipesPage } from "./recipesPage/RecipesPage";
+//context
+import { RecipeProvider } from "../context/recipe/RecipeProvider";
+
+export const Pages = () => {
+  return (
+    <RecipeProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" index element={<Home />} />
+          <Route path="recipes" element={<RecipesPage />} />
+          <Route path="recipes/:id" element={<RecipeDetail />} />
+        </Route>
+      </Routes>
+    </RecipeProvider>
+  );
+};
