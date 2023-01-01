e0a97985b71455beaa34bcb6c87843dc377c3c96
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-01-01 02:24:30 +0100

    refactor: resolve recipe provider import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from '../Layout';
+import { RecipeProvider } from './context/recipe/RecipeProvider';
 import { Home } from './pages/home/Home';
 import { RecipeDetail } from './pages/recipeDetail/RecipeDetail';
 import { RecipesPage } from './pages/recipesPage/RecipesPage';
